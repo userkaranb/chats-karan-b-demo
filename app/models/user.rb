00b63c62c1e00d8f9f3d4692e88ba346fd9143d1
Karan Bhatia <userkaranb@gmail.com>
2017-08-18 19:49:33 -0400

    fix bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,3 @@
 class User < ActiveRecord::Base
   has_many :messages
-  validate :username_not_empty
-
-  private
-
-  def username_not_empty
-  	binding.pry
-  	not username.nil? and not username.empty?
-  end
 end
